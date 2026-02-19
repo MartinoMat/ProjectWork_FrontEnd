66cb6c565d8c9a849cdd8d2cd3554eff94bce139
Matteo Martino <noobfp+github@gmail.com>
2026-02-19 14:56:15 +0100

    Pagina Prenota completata

--- pw_frontend/src/pages/Prenota.jsx
+++ pw_frontend/src/pages/Prenota.jsx
@@ -14,6 +14,7 @@ import {
 	addMonths,
 	subMonths
 } from 'date-fns';
+import { jwtDecode } from 'jwt-decode';
 import { it } from 'date-fns/locale';
 import goBack from '../img/back.png';
 import conf from '../img/confirm.png';
@@ -28,7 +29,9 @@ const Prenota = () => {
 	const [repartoScelto, setRepartoScelto] = useState(null);
 	const [esameScelto, setEsameScelto] = useState(null);
 	const [dataScelta, setDataScelta] = useState(null);
-	const [oraioScelto, setOrarioScelto] = useState(null);
+	const [orarioScelto, setOrarioScelto] = useState(null);
+	const [prenScelta, setPrenScelta] = useState(null);
+	const [prenotato, setPrenotato] = useState(null);
 
 	const monthStart = startOfMonth(currentMonth);
 	const monthEnd = endOfMonth(monthStart);
@@ -38,6 +41,9 @@ const Prenota = () => {
 	const days = eachDayOfInterval({ start: startDate, end: endDate });
 
 	const navigate = useNavigate();
+
+
+	let token = jwtDecode(localStorage.getItem('token'));
 
 	useEffect(() => {
 		const fetchDati = async () => {
@@ -81,10 +87,21 @@ const Prenota = () => {
 
 	const elencoOrari = (repartoScelto && esameScelto && dataScelta)
 		? esameScelto.prenotazione
-			.find(p => p.data === format(dataScelta, 'yyyy-MM-dd'), { locale: it })
+			.find(p => p.data === format(dataScelta, 'yyyy-MM-dd', { locale: it }))
 			?.orari.map(o => o.orario) || []
 		: [];
 
+{/*Ricerca di PrenotazioneId*/ }
+	function findPrenId(selezOra)
+	{
+		let prenid = (esameScelto && dataScelta && selezOra)
+			? esameScelto.prenotazione
+				.find(p => p.data === format(dataScelta, 'yyyy-MM-dd'))
+				?.orari.find(o => o.orario === selezOra)
+				?.prenotazioneId
+			: null;
+		setPrenScelta(prenid);
+	}
 
 {/*Gestieone dei select di Reparto ed Esame*/ }
 	const handleRepartoChange = (selectedOptionR) => {
@@ -99,7 +116,7 @@ const Prenota = () => {
 	if (loading) return <div>Caricamento in corso...</div>;
 
 
-{/*Metodo per impostare i colori nel calendario di backgfround e bordi*/ }
+{/*Metodo per impostare i colori nel calendario di background e bordi*/ }
 	const bgBorderData = (day, list = [], bg = true) => {
 		let color = '';
 		let isAvaiable = list.some(availableDay => isSameDay(new Date(availableDay), day));
@@ -115,7 +132,6 @@ const Prenota = () => {
 				}
 			}
 			else { color = '#f0f0f0'; }
-
 		}
 		else {
 
@@ -136,130 +152,191 @@ const Prenota = () => {
 		return color;
 	}
 
+
+{/*Preparazione e invio prenotaszione tramite httpPUT*/ }
+	const handlePrenota = async (e) => {
+		e.preventDefault();
+		var body = JSON.stringify({
+			prenotazioneId: prenScelta,
+			repartoId: repartoScelto.value,
+			esameId: esameScelto.value,
+			data: format(dataScelta, 'yyyy-MM-dd', { locale: it }),
+			orario: orarioScelto,
+			riservato: token.sub
+
+		});
+		try {
+			const response = await fetch('https://localhost:7036/Prenotazioni/Prenota', {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: body,
+			});
+			if (!response.ok) {
+				const errorData = response.json();
+				throw new Error(errorData.message || 'Errore durante l\'update');
+			}
+			setPrenotato('Prenotazione andata a buon fine!');
+
+		} catch (err) {
+			console.log(err.message);
+		}
+	};
+
 	return (
 		<div className="container">
 		{/*AREA SEMPRE PRESENTE*/}
 			< div className="center" >
 				<h1>Prenotazioni</h1>
 			</div >
-
-		{/*Bottone torna alla home*/}
-			< button
-				className="btn top l"
-				onClick={() => {
-					navigate('/home');
-				}}>
-				<img
-					className="top-btn-img"
-					src={goBack}
-					alt="Bottone torna indietro"
-				/>
+			
+				{/*Bottone torna alla home*/}
+				< button
+					className="btn top l"
+					onClick={() => {
+						navigate('/home');
+					}}>
+					<img
+						className="top-btn-img"
+						src={goBack}
+						alt="Bottone torna indietro"
+					/>
 			</button >
 
-		{/*Bottone Conferma*/}
-			<button className="btn top r conf">
-				<img
-					className="top-btn-img"
-					src={conf}
-					alt="Bottone conferma"
-				/>
-			</button>
+			{!prenotato && <div>
+				{/*Bottone Conferma*/}
+				{orarioScelto&&<button className="btn top r conf">
+					<img
+						className="top-btn-img"
+						src={conf}
+						alt="Bottone conferma"
+						onClick={handlePrenota}
+					/>
+				</button>}
 
-		{/*Zona selezione reparto*/}
-			<div className='input-row'>
-				<label className="al"><b>Reparto</b></label>
-				<Select
-					options={optionsReparti}
-					value={repartoScelto}
-					onChange={handleRepartoChange}
-					classNamePrefix="my-select"
-					className="my-select-container"
-				/>
-			</div>
-
-		{/*Zona selezione appuntamento*/}
-			<div className='input-row'>
-				<label className="al"><b>Esame</b></label>
-				<Select
-					options={optionsEsami}
-					value={esameScelto}
-					onChange={handleEsameChange}
-					isDisabled={!repartoScelto}
-					classNamePrefix="my-select"
-					className="my-select-container"
-				/>
-			</div>
-
-		{/*Container Calendario */}
-			<div className="container small" >
-				<div>
-					{isFuture(currentMonth, new Date()) && <button
-						className="btn top l"
-						onClick={() => setCurrentMonth(subMonths(currentMonth, 1))}>
-						<img
-							className="top-btn-img small flip"
-							src={arrow}
-							alt="Bottone mese precedente" />
-					</button>}
-
-					<div className='center'><h2>{format(currentMonth, 'MMMM yyyy', { locale: it })}</h2></div>
-					<button
-						className="btn top r"
-						onClick={() => setCurrentMonth(addMonths(currentMonth, 1))}>
-						<img
-							className="top-btn-img small"
-							src={arrow}
-							alt="Bottone mese successivo" /></button>
+				{/*Zona selezione reparto*/}
+				<div className='input-row'>
+					<label className="al"><b>Reparto</b></label>
+					<Select
+						options={optionsReparti}
+						value={repartoScelto}
+						onChange={handleRepartoChange}
+						classNamePrefix="my-select"
+						className="my-select-container"
+					/>
 				</div>
 
-			{/* Griglia dei giorni */}
-				<div className='cols'>
-					{['Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab', 'Dom'].map(d => (
-						<div key={d} className="daysNames">{d}</div>
-					))}
-
-					{days.map((day, idx) => (
-						<div
-							className='cell'
-							key={idx}
-							style={{
-								backgroundColor: bgBorderData(day, elencoDate),
-								border: bgBorderData(day, elencoDate, false)
-							}}
-							onClick={() => {
-								setDataScelta(day);
-								setOrarioScelto(null);
-							}}>
-							{format(day, 'd')}
-						</div>
-					))}
+				{/*Zona selezione appuntamento*/}
+				<div className='input-row'>
+					<label className="al"><b>Esame</b></label>
+					<Select
+						options={optionsEsami}
+						value={esameScelto}
+						onChange={handleEsameChange}
+						isDisabled={!repartoScelto}
+						classNamePrefix="my-select"
+						className="my-select-container"
+					/>
 				</div>
-			</div>
 
-		{/*Container Orario*/}
-			{dataScelta && <div className="container small">
-				{elencoOrari.length>0 && <div>
-					Per il <b>{format(dataScelta, 'dd MMMM yyyy', { locale: it })}</b> sono disponibili i seguenti orari: <br /><br />
-					<div className='cols ore'>
-						{elencoOrari.map((t, idy) => (
+				{/*Container Calendario */}
+				<div className="container small" >
+					<div>
+						{isFuture(currentMonth, new Date()) && <button
+							className="btn top l"
+							onClick={() => setCurrentMonth(subMonths(currentMonth, 1))}>
+							<img
+								className="top-btn-img small flip"
+								src={arrow}
+								alt="Bottone mese precedente" />
+						</button>}
+
+						<div className='center'><h2>{format(currentMonth, 'MMMM yyyy', { locale: it })}</h2></div>
+						<button
+							className="btn top r"
+							onClick={() => setCurrentMonth(addMonths(currentMonth, 1))}>
+							<img
+								className="top-btn-img small"
+								src={arrow}
+								alt="Bottone mese successivo" /></button>
+					</div>
+
+					{/* Griglia dei giorni */}
+					<div className='cols'>
+						{['Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab', 'Dom'].map(d => (
+							<div key={d} className="daysNames">{d}</div>
+						))}
+
+						{days.map((day, idx) => (
 							<div
-								className='cell ore'
-								onClick={() => { setOrarioScelto(t); }}
-								key={idy}
+								className='cell'
+								key={idx}
 								style={{
-									backgroundColor: '#ffffff',
-									border: oraioScelto === t ? '3px solid #ff4848' : '2px solid #dddddd'
+									backgroundColor: bgBorderData(day, elencoDate),
+									border: bgBorderData(day, elencoDate, false)
+								}}
+								onClick={() => {
+									setDataScelta(day);
+									setOrarioScelto(null);
 								}}>
-								{t.slice(0, 5)}
+								{format(day, 'd')}
 							</div>
 						))}
 					</div>
-				</div>}
-				{elencoOrari.length == 0 && <div>
-					Per il <b>{format(dataScelta, 'dd MMMM yyyy', { locale: it })} NON</b> sono disponibili i seguenti orari.
+				</div>
+
+				{/*Container Orario*/}
+				{dataScelta && <div className="container small">
+					{elencoOrari.length > 0 && <div>
+						Per il <b>{format(dataScelta, 'dd MMMM yyyy', { locale: it })}</b> sono disponibili i seguenti orari: <br /><br />
+						<div className='cols ore'>
+							{elencoOrari.map((t, idy) => (
+								<div
+									className='cell ore'
+									onClick={() => {
+										setOrarioScelto(t);
+										findPrenId(t);
+									}}
+									key={idy}
+									style={{
+										backgroundColor: '#ffffff',
+										border: orarioScelto === t ? '3px solid #ff4848' : '2px solid #dddddd'
+									}}>
+									{t.slice(0, 5)}
+								</div>
+							))}
+						</div>
+					</div>}
+					{elencoOrari.length == 0 && <div>
+						Per il <b>{format(dataScelta, 'dd MMMM yyyy', { locale: it })} NON</b> sono disponibili i seguenti orari.
+					</div>}
 				</div>}
 			</div>}
-		</div>
+
+		{/*Avviso di prenotazione avvenuta*/ }
+			{prenotato && <div className='center'>
+				<h3>{prenotato}</h3>
+				<div className='cont-pren'>
+					<div className='input-row-pren'>
+						<span className="pren-label">Reparto:</span>
+						<span className="pren-val"><b>{repartoScelto.label}</b></span>
+					</div>
+					<div className='input-row-pren'>
+						<span className="pren-label">Esame: </span>
+						<span className="pren-val"><b>{esameScelto.label}</b></span>
+					</div>
+					<div className='input-row-pren'>
+						<span className="pren-label">In data: </span>
+						<span className="pren-val"><b>{format(dataScelta, 'dd MMMM yyyy', { locale: it })}</b></span>
+					</div>
+					<div className='input-row-pren'>
+						<span className="pren-label">Alle Ore: </span>
+						<span className="pren-val"><b>{orarioScelto.slice(0,5)}</b></span>
+					</div>
+				</div>
+			</div>}
+	</div>
 	);
 };
 
